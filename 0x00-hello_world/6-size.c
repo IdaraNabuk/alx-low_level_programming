22be9a5c8216dca3c6cb8b930fef2c02d6d90250
IdaraNabuk <idaranabuk@gmail.com>
2022-10-13 18:19:05 +0000

    Edited more files

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
-/*
-  main - Entry point
-  Return: Always 0 (Success)
- */
-int main()
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
+ *     */
+int main(void)
 {
 		int a;
 			long int b;

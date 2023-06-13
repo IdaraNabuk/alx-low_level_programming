b9febac5268518ecdebc4ee2bbb3a7725dd93750
idaranabuk <idaranabuk@gmail.com>
2023-06-13 15:45:49 -0700

    Edits several files

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,21 +1,21 @@
 #include "main.h"
 
 /**
- * Recursive function that returns the length of a string.
+ * _strlen_recursion -  Recursive function that returns the length of a string.
  * @s: The string to be measured.
  *
  * Return: The length of the string.
  */
+
 int _strlen_recursion(char *s)
 {
-    int length = 0;
+	int length = 0;
 
-    // Base case: check if the current character is not null
-    if (*s != '\0')
-    {
-        length++;                           // Increment length for the current character
-        length += _strlen_recursion(s + 1);  // Recursive call with the next character
-    }
+	if (*s != '\0')
+	{
+		length++;
+		length += _strlen_recursion(s + 1);
+	}
 
-    return length;
+	return (length);
 }
--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,20 +1,23 @@
 #include "main.h"
 
 /**
- * Recursive function that returns the factorial of a given number.
+ * factorial -  Returns the factorial of a given number.
  * @n: The number to find the factorial of.
  *
  * Return: If n < 0, -1 to indicate an error for negative numbers
- * 	   If n == 0, 1 as the base case for factorial of 0
- * 	   If n > 0, Recursive call to calculate factorial
+ *	If n == 0, 1 as the base case for factorial of 0
+ *	If n > 0, Recursive call to calculate factorial
  */
 
-int factorial(int n) {
-  if (n < 0) {
-    return -1;  // Return -1 to indicate an error for negative numbers
-  } else if (n == 0) {
-    return 1;   // Return 1 as the base case for factorial of 0
-  } else {
-    return n * factorial(n - 1);  // Recursive call to calculate factorial
-  }
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	} else if (n == 0)
+	{
+		return (1);
+	}
+
+	return (n * factorial(n - 1));
 }

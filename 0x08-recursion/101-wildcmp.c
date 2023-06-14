91f68701b3fc7bdb0c8bfdaeffd3ea1025ad0e6a
idaranabuk <idaranabuk@gmail.com>
2023-06-14 15:10:21 -0700

    Edits 1 file

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,57 +1,41 @@
 #include "main.h"
 
 /**
-* wildcmp - Compares two strings, considering wildcard characters
-* @s1: First string to be compared
-* @s2: Second string to be compared
-*
-* Return: 1 if both strings are empty (considered identical)
-*	Call helper function
-*/
+ * wildcmp - Compares two strings and checks
+ * if they can be considered identical
+ * @s1: The first string to compare.
+ * @s2: The second string to compare (may contain wildcard character '*').
+ *
+ * Return: 1 if the strings can be considered identical, 0 otherwise.
+ */
 
 int wildcmp(char *s1, char *s2)
 {
+	/* If both strings are empty, they are considered identical */
 	if (*s1 == '\0' && *s2 == '\0')
-	{
 		return (1);
-	}
 
-	return (compare(s1, s2));
-}
-
-/**
-* compare - Compares and checks the next character
-* @s1: First string to be compared
-* @s2: Second string to be compared
-*
-* Return: 1 if the remaining of s2 is only '*'
-*	0 if no match is found
-*	0 if characters don't match or one of the strings ends
-*/
-
-bool compare(char *s1, char *s2)
-{
+	/* If s2 is the wildcard character '*', check for all possibilities */
 	if (*s2 == '*')
 	{
+		/* If the remaining of s2 is only '*', consider it identical */
 		if (*(s2 + 1) == '\0')
-		{
 			return (1);
-		}
-		while (*s1 != '\0')
-		{
-			if (wildcmp(s1, s2 + 1))
-			{
-				return (1);
-			}
-			s1++;
-		}
 
-		return (0);
+		/* Recursive calls to check all possibilities */
+		if (*s1 != '\0' && wildcmp(s1 + 1, s2) == 1)
+			return (1);
+		if (wildcmp(s1, s2 + 1) == 1)
+			return (1);
 	}
-	if (*s1 == '\0' || *s2 == '\0' || *s1 != *s2)
+
+	/* If characters match or one of the strings ends, continue the comparison */
+	if (*s1 == *s2)
 	{
-		return (0);
+		if (*s1 == '\0')
+			return (1);
+		return (wildcmp(s1 + 1, s2 + 1));
 	}
 
-	return (compare(s1 + 1, s2 + 1));
+	return (0); /* Strings are not identical */
 }

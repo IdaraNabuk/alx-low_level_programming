4e82c635ad2d04b31dd29c1c0e5ef214ff39c9e8
idaranabuk <idaranabuk@gmail.com>
2023-06-23 05:35:20 -0700

    Adds 1 file

---
+++ 0x09-static_libraries/100-atoi.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _atoi - Converts a string to an integer.
+ * @s: String to be converted.
+ *
+ * Return: Integer value of the converted string.
+ */
+
+int _atoi(char *s)
+{
+
+}

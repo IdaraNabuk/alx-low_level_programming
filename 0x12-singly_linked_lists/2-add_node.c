e477d573941ef462559cf0aefab2dfb5590f29a2
idaranabuk <idaranabuk@gmail.com>
2023-07-06 15:06:10 -0700

    Edits 1 file

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -11,20 +11,20 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
+	size_t nchar;
 
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
-	return (NULL);
+		return (NULL);
 
 	new_node->str = strdup(str);
-	if (new_node->str == NULL)
-	{
-		free(new_node);
-		return (NULL);
-	}
 
+	for (nchar = 0; str[nchar]; nchar++)
+	;
+
+	new_node->len = nchar;
 	new_node->next = *head;
 	*head = new_node;
 
-	return (new_node);
+	return (*head);
 }
